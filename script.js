7bf797cb16efdbd3b737c19a7752d76005cd62f1
Steeln <steelnca@gmail.com>
2024-08-07 22:00:23 +0100

    FIRST COMMIT

---
+++ script.js
@@ -0,0 +1,37 @@
+
+document.getElementById('menu-btn').addEventListener('click', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
+    mobileMenu.classList.toggle('hidden');
+    mobileMenuOverlay.classList.toggle('hidden');
+});
+
+document.getElementById('mobile-menu-overlay').addEventListener('click', () => {
+  const mobileMenu = document.getElementById('mobile-menu');
+  const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
+  mobileMenu.classList.toggle('hidden');
+  mobileMenuOverlay.classList.toggle('hidden');
+});
+
+const toggleThemeButtons = document.querySelectorAll('.theme-toggle');
+const body = document.body;
+
+
+// Load theme from local storage
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark');
+} else {
+  body.classList.remove('dark');
+}
+
+// Toggle theme on button click
+toggleThemeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+    });
+});
